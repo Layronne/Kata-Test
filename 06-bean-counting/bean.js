1c23e0992b0016e92e7e1d9849f24e38c09b6676
Layronne <92851704+Layronne@users.noreply.github.com>
2021-12-17 11:00:19 +0100

    Update bean.js

---
+++ 06-bean-counting/bean.js
@@ -0,0 +1,22 @@
+function countBs(char) {
+  var count = 0;
+  for (let i = 0; i < char.length; i++) {
+    if (char.charAt(i) == "B") {
+      count++;
+    }
+  }
+  return count;
+}
+
+
+
+
+function countChar(char1, char2) {
+  var count = 0;
+  for (let i = 0; i < char1.length; i++) {
+    if (char1.charAt(i) == char2) {
+      count++;
+    }
+  }
+  return count;
+}
