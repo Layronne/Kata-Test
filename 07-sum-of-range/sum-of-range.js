abd63621d7bba604f0b19c93d1dfe230c472dc6d
Layronne <92851704+Layronne@users.noreply.github.com>
2021-12-17 14:32:03 +0100

    Update sum-of-range.js

--- 07-sum-of-range/sum-of-range.js
+++ 07-sum-of-range/sum-of-range.js
@@ -1,15 +1,16 @@
+
 function range(firstNbr, lastNbr) {
   let rangeNbr = [];
-  for (let i = 0; i < lastNbr; i++){
+  for (let i = firstNbr; i <= lastNbr; i++){
     rangeNbr[i] = firstNbr++;
  }
   return rangeNbr;
 }
 
 function sum(firstNbr, lastNbr) {
-  let rangeNbr = []
-  let add = 0;
-  for (let i = 0; i < lastNbr; i++){
+  let rangeNbr = [];
+  let add = firstNbr;
+  for (let i = firstNbr; i <= lastNbr; i++){
     rangeNbr[i] = firstNbr++;
  }
   for (let i = 0; i < rangeNbr.length; i++) {
